df30ebfebffce8404db03e5556981c9e42265fbb
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-15 01:17:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,16 @@
 
+def turn_count(board)
+  counter = 0
 
+  board.each do |turn|
+    if turn.downcase == "x" || turn.downcase == "o"
+      counter += 1
+      puts "#{counter}"
+    end
+  end
+
+  counter
+end
 
     
     
