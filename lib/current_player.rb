df89868eeb62f5b4b81adbebb8f255eb9f710a49
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-15 00:51:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,13 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def turn_player(board)
-  if board.each do |board| 
+  board.each do |board| 
     board % 2 == 0 
       puts "X"
     board % 2 == 1 
       puts "O"
+  end
+end
       
     
   
