675861ae604f2cb2aeaf610555ca0cabbe9937e0
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-15 01:12:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,8 +3,8 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0
   board.each do |turn|
-  end
-  if board = "X" || board = "O"
+ 
+  if turn = "X" || turn = "O"
     counter += 1 
   end
 end
