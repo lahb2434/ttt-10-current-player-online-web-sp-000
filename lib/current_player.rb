34372d3ccba73bafc92cec0b23206d5175b07f52
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-15 00:47:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,5 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_player(board)
-  if board.even?
-    return "X"
-  else board.odd?
-    return "O"
-  end
-end
+  if board.each
   
   
